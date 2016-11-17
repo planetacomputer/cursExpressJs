b7c4a95871e3ddf83109679ee61d45300fe76c93
planetacomputer <evapardeza123@gmail.com>
2016-11-17 15:15:14 +0100

    Express js MVC con vistas ejs, router

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+var things = require('./things.js'); 
+
+
+//Simple request time logger
+app.use('/hotel', function(req, res, next){
+	console.log("A new request received at " + Date.now());
+	//This function call is very important. It tells that more processing is 
+	//required for the current request and is in the next middleware function/route handler.
+	next();
+});
+
+//both index.js and things.js should be in same directory
+app.use('/', things); 
+
+app.listen(3001, function() {
+  console.log('Servidor funcionando en http://localhost:3001');
+});
